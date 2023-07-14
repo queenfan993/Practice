d93457ceb0282e568933f510349ce26525735cf9
queenfan93 <queenfan93@gmail.com>
2023-07-14 21:58:12 +0800

    Prac

--- c/Basic/test.c
+++ c/Basic/test.c
@@ -17,17 +17,19 @@ void pointerToPointer(char** str) {
 
 
 int main() {
-    
+    char* arr1[] = {"hello", "world"};
     char str[] = "<This></is></a></test>";
     //pointer(str);
 
-    char matrix[2][20] = {
+    char matrix[2][10] = {
   		"{1, 2}",
   		"{2, 3}"
   	};
-  	/*
-  	printf("%s\n", matrix[0]);
-  	printf("%s\n", matrix[1]);
+  	printf("strlen:%d\n", strlen(matrix[0])); //會印出 6
+  	printf("matrix[0]: %p\n", &matrix[0]); 
+    printf("matrix[0][1]: %p\n", &matrix[0][1]); // 和
+  	printf("matrix[1]: %p\n", &matrix[1]); // 和 matrix[0] 差 10 個字節 = 10 char 大小 
+    /*
   	char* matrics[] = { matrix[0], matrix[1] };
   	printf("%s\n", matrics[0]);
   	printf("%s\n", matrics[1]);
@@ -37,8 +39,11 @@ int main() {
   		A[i] = (char*)calloc(5, sizeof(char));
   		sprintf(A[i], "%d, %d", i, i+1);
   	}
-  	printf("%s\n", A[0]);
-  	printf("%s\n", A[1]);
+    printf("%d\n", sizeof(int*));
+  	printf("A[0]:%p\n", A[0]);
+    printf("A[0][0]:%p\n", A[0][0]); 
+    printf("A[0][1]:%p\n", A[0][1]);
+  	printf("A[1]:%p\n", A[1]); 
 	
   	pointerToCharArray(A);
   	//pointerToCharArray(matrix);
